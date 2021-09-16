74b36db833a8b22bcaca2c49d2c7994ecfd0af3f
Ayush Srivastav <61233311+ayushsrivastav0811@users.noreply.github.com>
2021-09-16 21:56:14 +0530

    Create two-sum.cpp

---
+++ Day-04/Ayush/two-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+{
+    //Key is the number and value is its index in the vector.
+	unordered_map<int, int> hash;
+	vector<int> result;
+	for (int i = 0; i < numbers.size(); i++) {
+		int numberToFind = target - numbers[i];
+
+     
+		if (hash.find(numberToFind) != hash.end()) {
+                    
+			result.push_back(hash[numberToFind] );
+			result.push_back(i );			
+			return result;
+		}
+
+
+		hash[numbers[i]] = i;
+	}
+	return result;
+
+    }
+};
