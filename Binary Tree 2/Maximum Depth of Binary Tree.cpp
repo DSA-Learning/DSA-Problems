be7ef4a126e2f3d26a49831e5489b85be7e8db77
Ayush Srivastav <61233311+ayushsrivastav0811@users.noreply.github.com>
2022-07-17 14:33:51 +0530

    Update Maximum Depth of Binary Tree.cpp

--- Binary Tree 2/Maximum Depth of Binary Tree.cpp
+++ Binary Tree 2/Maximum Depth of Binary Tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+* DFS Solution
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
@@ -18,5 +19,32 @@ public:
         int right=maxDepth(root->right);
         
         return 1+max(left,right);
+    }
+};
+
+* BFS Solution
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        queue<TreeNode *>q;
+        q.push(root);
+        int c=0;
+        while(q.empty()==false)
+        {
+            int size=q.size();
+            c++;
+            while(size--)
+            {
+                TreeNode *curr=q.front();
+                q.pop();
+                if(curr->left)
+                    q.push(curr->left);
+                if(curr->right)
+                    q.push(curr->right);
+            }
+        }
+        return c;
     }
 };
